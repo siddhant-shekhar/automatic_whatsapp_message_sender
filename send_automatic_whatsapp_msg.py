f1bb7fe4ece72c3807ded1d8e132fefaea19b009
Siddhant <104164130+siddhant-shekhar@users.noreply.github.com>
2022-11-08 01:14:46 +0530

    Update send_automatic_whatsapp_msg.py

--- send_automatic_whatsapp_msg.py
+++ send_automatic_whatsapp_msg.py
@@ -3,8 +3,19 @@ import time
 
 time.sleep(5)
 count=0 
-while count<=2:
-    #pyautogui.click()
-    pyautogui.typewrite("hii" + str(count))
+while count<=n:   # n= number of times you want to send message
+    #pyautogui.click() / you can use this if you want to automate clicks. For  specific position you need to specify location(x and y coordinates)
+    pyautogui.typewrite("Your Message")
     pyautogui.press("enter")
     count=count+1
+    
+_________________________________________________________________________________________________________________________________________________________________________
+
+#example:    n=2
+#            pyautogui.typewrite("Hii" + str(count))
+
+#output:     Hii 0
+#            Hii 1
+#            Hii 2
+  
+    
